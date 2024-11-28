d5653bf875aedc32517fe3539ad0bf98b72a31e0
SanjuMu <sanjusri.munigala@gmail.com>
2024-11-27 19:34:10 -0500

    divisble_number

---
+++ divisble_number.py
@@ -0,0 +1,13 @@
+print("Enter a number(Numerator): ")
+num1 = int(input())
+print("Enter a Number (Denominator):")
+num2 = int(input())
+
+if num1%num2 == 0:
+    print("\n" +str(num1)+ " is divisible by " + str (num2))
+          
+else:
+    print("\n "+str(num1)+" is not divisible by " + str (num2))
+          
+
+          
