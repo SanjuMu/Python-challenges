0181b9674f23c13c94643c8a9b8b8477ca34997f
SanjuMu <sanjusri.munigala@gmail.com>
2024-11-27 19:23:45 -0500

    opertor_precedence

---
+++ opertor_precedence.py
@@ -0,0 +1,18 @@
+v = 2
+w = 5
+x = 10
+y= 8
+z = 1
+
+x = (v+w)*x/w
+print("The Value of (v+w)*x/w is ", x)
+
+
+name = "Sanju"
+age = 13
+
+if name == "Sanju" or name == "Bob" and age >= 2:
+    print("Hello,Welcome")
+    
+else :
+    print("Good bye!!")
